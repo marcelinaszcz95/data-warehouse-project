07b8d4630bad7047394ed0f0be25a83546a6e936
Marcelina <160395155+marcelinaszcz95@users.noreply.github.com>
2025-02-20 19:20:02 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -21,7 +21,7 @@ GO
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
   ALTER DATABASE DataWarehouse SET SINGLE USER WITH ROLLBACK IMMEDIATE;
-  DROP DATABASE DataWarehosue;
+  DROP DATABASE DataWarehouse;
 END;
 GO
 
